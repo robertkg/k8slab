fd2ff3389889b1d7e65a53be3e6bae374351cc1a
robertkg <rgranmo@gmail.com>
2024-01-26 16:39:21 +0100

    Update setup.sh
    - Fix argocd cli exit code
    - Fix argocd deployment wait
    - Add argocd login in script
    - Output argocd login details

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 kubectl version || exit 1
-argocd version || exit 1
+which argocd    || exit 1 # CLI throws error when login is not set up
 
 echo "=== Installing ArgoCD ==="
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || exit 1
 
 echo "Waiting for ArgoCD deployment ready state..."
-kubectl wait deployment/argocd-server --for=condition=Available --timeout=300s
-
-ARGOCD_ADMIN_SECRET="$(kubectl get secret -n argocd argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 -d)" || exit 1
-#ARGOCD_SERVER="$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')"
+kubectl wait -n argocd deployment/argocd-server --for=condition=Available --timeout=300s
 
 echo " "
 echo "Please port set up a port forward from another shell and log into ArgoCD"
 echo " "
 echo "  kubectl port-forward -n argocd svc/argocd-server 8080:443"
-echo "  argocd login localhost:8080 --username admin --password ${ARGOCD_ADMIN_SECRET}"
-read -p "Press Enter to continue..."
+read -r -p "Press Enter to continue..."
+
+echo "Logging into ArgoCD server on localhost:8080"
+ARGOCD_ADMIN_SECRET="$(kubectl get secret -n argocd argocd-initial-admin-secret -o json | jq -r '.data.password' | base64 -d)" || exit 1
+argocd login localhost:8080 --username admin --password "${ARGOCD_ADMIN_SECRET}"
 
 echo "=== Creating ArgoCD app: vault ==="
 argocd proj create -f manifest/argocd/vault/project.yaml --upsert
@@ -32,5 +32,11 @@ kubectl wait -n prometheus deployment/prometheus-grafana --for=condition=Availab
 echo "=== Creating ArgoCD app: example-app ==="
 argocd proj create -f manifest/argocd/example-app/project.yaml --upsert
 argocd app create -f manifest/argocd/example-app/application.yaml --upsert
+
+echo " "
+echo "ArgoCD login details:"
+echo "  server address: https://localhost:8080"
+echo "  username: admin"
+echo "  password: ${ARGOCD_ADMIN_SECRET}"
 
 echo "Done"
